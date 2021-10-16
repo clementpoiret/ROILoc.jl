ea907b813c7bce38d41055e817ce56bbad731ead
Clément POIRET <poiret.clement@outlook.fr>
2021-10-16 12:15:08 +0200

    [CODE] Load MRIs ad NIVolumes

--- src/load.jl
+++ src/load.jl
@@ -3,7 +3,7 @@ using NIfTI
 
 
 """
-    glob_mris(path, pattern)
+    load_mris(path, pattern)
 
 Get a list of MRIs in the given path.
 
@@ -14,25 +14,32 @@ Arguments
 
 Returns
 -------
-- `mris::Vector{String}`: List of MRIs.
+- `mris::Vector{String}`: List of paths of MRIs.
+- `loaded_mris::Vector{NIVolume}`: List of loaded MRIs.
 
 Example
 _______
 
 ```julia-repl
-julia> glob_mris("~/Datasets/", "**/tse.nii.gz")
+julia> paths, volumes = load_mris("~/Datasets/", "**/tse.nii.gz");
+julia> paths
 1-element Vector{String}:
- "~/Datasets/sub-00C/tse.nii.gz"
+ "/home/me/Datasets/sub-00C/tse.nii.gz"
+julia> volumes
+1-element Vector{NIVolume}:
+ Float32[20.0 83.0 … 63.0 19.0; 10.0 59.0 …
 ```
 """
-function glob_mris(path::String, pattern::String)::Vector{String}
+function load_mris(path::String,
+                   pattern::String)::Tuple{Vector{String},Vector{NIVolume}}
     # todo: assert pattern compliant with nifti files
     path = expanduser(path)
 
     mris = glob(pattern, path)
     if (length(mris) == 0)
         error("No MRIs found in path: ", path)
+    else
+        loaded_mris = [niread(mri) for mri in mris]
+        return mris, loaded_mris
     end
-
-    return mris
 end
